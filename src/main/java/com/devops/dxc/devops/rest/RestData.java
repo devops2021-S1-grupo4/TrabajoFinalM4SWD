3643ad1bdad77d28c8efac2793591ddcc3a1150f
Hernan Moreno <hernan.alexis.moreno@gmail.com>
2022-03-24 16:46:20 -0300

    Se agrega cors para que front pueda consumir los servicios

--- src/main/java/com/devops/dxc/devops/rest/RestData.java
+++ src/main/java/com/devops/dxc/devops/rest/RestData.java
@@ -8,6 +8,7 @@ import com.devops.dxc.devops.model.Impuesto;
 import com.devops.dxc.devops.model.SaldoAhorro;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,7 @@ public class RestData {
 
 	private final static Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
 
+	@CrossOrigin
 	@GetMapping(path = "/dxc", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Dxc getData(@RequestParam(name = "sueldo") String sueldo, @RequestParam(name = "ahorro") String ahorro){
 
@@ -29,6 +31,7 @@ public class RestData {
 		return response;
 	}
 
+	@CrossOrigin
 	@GetMapping(path = "/saldoahorro", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody SaldoAhorro getSaldorestante(@RequestParam(name = "ahorro") String ahorro){
 
@@ -38,6 +41,7 @@ public class RestData {
 		return response;
 	}
 
+	@CrossOrigin
 	@GetMapping(path = "/impuesto", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Impuesto getImpuesto(@RequestParam(name = "sueldo") String sueldo){
 
